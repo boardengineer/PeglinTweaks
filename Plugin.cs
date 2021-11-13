98b2189f2fc387b35b0fd28a671b909bba505f91
bo0tzz <btdewilligen@gmail.com>
2021-11-13 12:19:36 +0100

    Move relic patches into separate namespace

--- Plugin.cs
+++ Plugin.cs
@@ -1,8 +1,5 @@
 ﻿using BepInEx;
 using HarmonyLib;
-using System.Collections.Generic;
-using System.Reflection;
-using System.Reflection.Emit;
 
 namespace PeglinTweaks
 {
@@ -18,30 +15,6 @@ namespace PeglinTweaks
 
             harmony.PatchAll();
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
-        }
-    }
-
-    [HarmonyPatch(typeof(Battle.PegManager), "ResetPeg")]
-    public class CookbookChancePatch
-    {
-        private static readonly FieldInfo chanceField = AccessTools.DeclaredField(typeof(Relics.RelicManager), "PEG_TO_BOMB_CHANCE");
-
-        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-        {
-            foreach (var instruction in instructions)
-            {
-                if (instruction.LoadsField(chanceField))
-                {
-                    //There's an object we don't need on the stack, so pop it
-                    yield return new CodeInstruction(OpCodes.Pop);
-                    //Push a float32 onto the stack, using the value from our config
-                    yield return new CodeInstruction(OpCodes.Ldc_R4, Configuration.CookbookBombChance);
-                }
-                else
-                {
-                    yield return instruction;
-                }
-            }
         }
     }
 }
---
+++ Relics.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using Relics;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace PeglinTweaks.Relic
+{
+    [HarmonyPatch(typeof(Battle.PegManager), "ResetPeg")]
+    public class CookbookChancePatch
+    {
+        private static readonly FieldInfo chanceField = AccessTools.DeclaredField(typeof(RelicManager), "PEG_TO_BOMB_CHANCE");
+
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            foreach (var instruction in instructions)
+            {
+                if (instruction.LoadsField(chanceField))
+                {
+                    //There's an object we don't need on the stack, so pop it
+                    yield return new CodeInstruction(OpCodes.Pop);
+                    //Push a float32 onto the stack, using the value from our config
+                    yield return new CodeInstruction(OpCodes.Ldc_R4, Configuration.CookbookBombChance);
+                }
+                else
+                {
+                    yield return instruction;
+                }
+            }
+        }
+    }
+}
