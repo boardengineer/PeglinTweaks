5ecdd2f0365a5996407a3e4e6da836d17d0bb8ba
bo0tzz <btdewilligen@gmail.com>
2021-11-13 12:13:38 +0100

    Create tweaks for pachinko aimer
    Closes #10

--- Configuration.cs
+++ Configuration.cs
@@ -4,16 +4,28 @@ namespace PeglinTweaks
 {
     class Configuration
     {
+        //Relics
         private static ConfigEntry<float> cookbookBombChanceCfg;
+
+        //Pachinko
+        private static ConfigEntry<int> aimerLengthCfg;
+        private static ConfigEntry<int> aimerBouncesCfg;
 
         public static void BindConfigs(ConfigFile config)
         {
+            //Relics
             cookbookBombChanceCfg = config.Bind("Relics", "Cookbook_Bomb_Chance", 0.07F, "Alchemist's cookbook bomb conversion chance. Use a value between 0 and 1");
+
+            //Pachinko
+            aimerLengthCfg = config.Bind("Pachinko", "Aimer_Length", 35, "The length of the aiming reticle when firing");
+            aimerBouncesCfg = config.Bind("Pachinko", "Aimer_Bounces", 1, "The amount of bounces that the aiming reticle shows");
         }
 
-        public static float CookbookBombChance
-        {
-            get { return cookbookBombChanceCfg.Value; }
-        }
+        //Relics
+        public static float CookbookBombChance => cookbookBombChanceCfg.Value;
+
+        //Pachinko
+        public static int AimerLength => aimerLengthCfg.Value;
+        public static int AimerBounces => aimerBouncesCfg.Value;
     }
 }
---
+++ Pachinko.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+
+namespace PeglinTweaks.Pachinko
+{
+    [HarmonyPatch(typeof(PredictionManager), "Initialize")]
+    class PredictionManagerPatch
+    {
+        public static void Prefix(ref int ____baseMaxIterations)
+        {
+           ____baseMaxIterations = Configuration.AimerLength;
+        }
+
+        public static void Postfix(ref int ____bounceCount)
+        {
+            ____bounceCount = Configuration.AimerBounces;
+        }
+    }
+}
